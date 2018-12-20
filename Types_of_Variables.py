71d290674e9ecb5684a61d84807cd7fdc9818f3d
Cognitior-Tech <46032757+Cognitior-Tech@users.noreply.github.com>
2018-12-21 00:49:40 +0530

    Add files via upload

---
+++ Types_of_Variables.py
@@ -0,0 +1,33 @@
+#Class 1 - An Introduction to Python 
+
+"""Variables"""
+
+#Float / Double
+Sales = 110.10
+Profit=25
+
+#Integer 
+Quantity = 20
+
+#String
+Category = 'Furniture'
+
+Category = "Furniture"
+
+#Logical
+IS_Sales_More=(Sales>100)
+
+
+#Arithmetic
+Profit_Ratio =Sales/Profit
+
+
+greeting="Hello"
+Name="Learners"
+Message= "Welcome to the Python Tutorial" 
+
+message=greeting+Name+Message
+
+message=greeting + " " + Name + " " + Message
+
+message
